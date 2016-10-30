9e9c6f6746a6b32ee771bd6b049f15c5974040e2
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-10-30 23:05:57 +0200

    GenericList partly implemented.

---
+++ OOP_HomeWorks/DefiningClassesPart2/GenericClass/GenericList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace GenericClass
+{
+    public class GenericList<T>
+    {
+        private T[] elementsArray;
+        private int currentIndex;
+
+        public GenericList(int initialSize = 16)
+        {
+            this.ElementsArray = new T[initialSize];
+            this.CurrentIndex = -1;
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.ElementsArray.Length;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.CurrentIndex;
+            }
+        }
+
+        private T[] ElementsArray
+        {
+            get
+            {
+                return this.elementsArray;
+            }
+
+            set
+            {
+                this.elementsArray = value;
+            }
+        }
+
+        public int CurrentIndex
+        {
+            get
+            {
+                return this.currentIndex;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    this.currentIndex = 0;
+                }
+                else
+                {
+                    this.currentIndex = value;
+                }
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (this.Count == 0 || index > this.currentIndex || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return this.ElementsArray[index];
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (this.Count == this.Capacity)
+            {
+                this.AutoGrow();
+            }
+
+            this.ElementsArray[this.CurrentIndex] = element;
+            this.CurrentIndex++;
+        }
+
+        private void AutoGrow()
+        {
+            T[] newArray = new T[this.Capacity * 2];
+
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                newArray[i] = this.ElementsArray[i];
+            }
+
+            this.ElementsArray = newArray;
+        }
+    }
+}
---
+++ OOP_HomeWorks/DefiningClassesPart2/GenericClass/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericClass
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GenericList<string> list = new GenericList<string>();
+
+            //list.Add("Pesho");
+            //list.Add("Pesho");
+            //list.Add("Penka");
+
+            System.Console.WriteLine("count - {0}", list.Count);
+            System.Console.WriteLine("capacity - {0}", list.Capacity);
+
+
+            System.Console.WriteLine(list[0]);
+        }
+    }
+}
