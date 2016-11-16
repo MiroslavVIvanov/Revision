89ad72167e3543f545d4a6f8ff40e36b7fe41064
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-11-16 13:09:09 +0200

    Formatting code homework from the HQC course - done

---
+++ HQC_HomeWorks/FormattingCode/FormattingCode/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace FormattingCode
+{
+    using System;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            while (Engine.ExecuteNextCommand())
+            {
+            }
+
+            Console.WriteLine(Messages.Output);
+        }
+    }
+}
---
+++ HQC_HomeWorks/FormattingCode/FormattingCode/EventHolder.cs
@@ -0,0 +1,50 @@
+﻿namespace FormattingCode
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Event> storedByTitle = new MultiDictionary<string, Event>(true);
+        private OrderedBag<Event> sortedByDate = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+
+            this.storedByTitle.Add(title.ToLower(), newEvent);
+            this.sortedByDate.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+            foreach (var eventToRemove in this.storedByTitle[title])
+            {
+                removed++;
+                this.sortedByDate.Remove(eventToRemove);
+            }
+
+            this.storedByTitle.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = 
+                this.sortedByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                Messages.PrintEvent(eventToShow);
+            }
+
+            if (eventsToShow.Count == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
