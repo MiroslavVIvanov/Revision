c7c14e3ff6b299ab85da46bb141d683bde8fc8f6
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-11-17 16:39:42 +0200

    Variables Data Expressions Constants homework for the HQC course - done

---
+++ HQC_HomeWorks/VariablesExpressionsConstants/ShapesSystem/Size.cs
@@ -0,0 +1,65 @@
+﻿namespace ShapesSystem
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width can not be negative!");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height can not be negative!");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public static Size CalculateRotatedFigureSize(Size size, double rotationAngle)
+        {
+            var newWidth = (Math.Abs(Math.Cos(rotationAngle)) * size.Width) +
+                    (Math.Abs(Math.Sin(rotationAngle)) * size.Height);
+
+            var newHeight = (Math.Abs(Math.Sin(rotationAngle)) * size.Width) +
+                    (Math.Abs(Math.Cos(rotationAngle)) * size.Height);
+
+            var rotatedFigureSize = new Size(newWidth, newHeight);
+
+            return rotatedFigureSize;
+        }
+    }
+}
---
+++ HQC_HomeWorks/VariablesExpressionsConstants/StatisticsPrinter/ConsolePrinter.cs
@@ -0,0 +1,96 @@
+﻿namespace StatisticsPrinter
+{
+    using System;
+
+    public class ConsolePrinter
+    {
+        public void PrintFullStatisticsReport(double[] statistics)
+        {
+            double maxValue = this.FindMaxValue(statistics);
+            this.Print(maxValue);
+            
+            double minValue = this.FindMinValue(statistics);
+            this.Print(minValue);
+
+            double averageValue = this.CalculateAverageValue(statistics);
+            this.Print(averageValue);
+        }
+
+        private double FindMaxValue(double[] statistics)
+        {
+            double maxValue = double.MinValue;
+            for (int i = 0; i < statistics.Length; i++)
+            {
+                if (maxValue > statistics[i])
+                {
+                    maxValue = statistics[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        private double FindMinValue(double[] statistics)
+        {
+            double minValue = double.MaxValue;
+            for (int i = 0; i < statistics.Length; i++)
+            {
+                if (minValue < statistics[i])
+                {
+                    minValue = statistics[i];
+                }
+            }
+
+            return minValue;
+        }
+
+        private double CalculateAverageValue(double[] statistics)
+        {
+            double sumOfAllElements = 0;
+            for (int i = 0; i < statistics.Length; i++)
+            {
+                sumOfAllElements += statistics[i];
+            }
+
+            double average = sumOfAllElements / (double)statistics.Length;
+            return average;
+        }
+
+        private void Print(double result)
+        {
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
+
+// Version of the method before refactoring
+
+////public void PrintStatistics(double[] arr, int count)
+////{
+////    double max, tmp;
+////    for (int i = 0; i < count; i++)
+////    {
+////        if (arr[i] > max)
+////        {
+////            max = arr[i];
+////        }
+////    }
+////    PrintMax(max);
+////    tmp = 0;
+////    max = 0;
+////    for (int i = 0; i < count; i++)
+////    {
+////        if (arr[i] < max)
+////        {
+////            max = arr[i];
+////        }
+////    }
+////    PrintMin(max);
+  
+////    tmp = 0;
+////    for (int i = 0; i < count; i++)
+////    {
+////        tmp += arr[i];
+////    }
+////    PrintAvg(tmp / count);
+////}
