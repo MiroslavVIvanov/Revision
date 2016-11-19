bc1a6a1c799837d16a73103958eef526626a7149
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-11-19 17:03:24 +0200

    Abstraction project refactored

---
+++ HQC_HomeWorks/HighQualityClasses/Abstraction/Circle.cs
@@ -0,0 +1,26 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    public class Circle : Figure
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+        public override double CalculateSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+    }
+}
---
+++ HQC_HomeWorks/HighQualityClasses/Abstraction/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    public class Rectangle : Figure
+    {
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public override double CalculateSurface()
+        {
+            double surface = this.Width * this.Height;
+            return surface;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double perimeter = 2 * (this.Width + this.Height);
+            return perimeter;
+        }
+    }
+}
