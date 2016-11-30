b39e7dca134627928c01417c0c5780a91be4d104
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-11-30 14:21:36 +0200

    Added Position.cs without integrating it to old code.

---
+++ HQC_HomeWorks/RefactorigCode/RefactoredCode/Position.cs
@@ -0,0 +1,50 @@
+﻿namespace MatrixWalk
+{
+    using System;
+
+    public class Position
+    {
+        private int row;
+        private int col;
+
+        public Position(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Row can't be less than 0");
+                }
+
+                this.row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return this.col;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Column can't be less than 0");
+                }
+
+                this.col = value;
+            }
+        }
+    }
+}
